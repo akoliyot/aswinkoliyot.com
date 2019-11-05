c1c33b7ff648da91910e01c85cf13d884e2a29f8
Aswin Koliyot <akoliyot@gmail.com>
2019-11-05 22:04:16 +0100

    Add basic styles to Post

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,7 +9,7 @@
 const path = require(`path`);
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions;
-  const blogPostTemplate = path.resolve(`src/templates/blogTemplate.tsx`);
+  const blogPostTemplate = path.resolve(`src/templates/PostTemplate.tsx`);
   const result = await graphql(`
     {
       allMdx(sort: { order: DESC, fields: [frontmatter___date] }, limit: 1000) {
--- src/templates/PostTemplate.tsx
+++ src/templates/PostTemplate.tsx
@@ -3,6 +3,36 @@ import { graphql } from "gatsby";
 import Img from "gatsby-image";
 import { MDXRenderer } from "gatsby-plugin-mdx";
 import { AppWrapper } from "../components/AppWrapper";
+import styled from "styled-components";
+
+const StyledPost = styled.div`
+  padding: 15px;
+
+  p {
+    text-align: justify;
+    line-height: 1.7;
+  }
+
+  ul {
+    padding: 0;
+    line-height: 1.7;
+
+    li {
+      list-style-type: circle;
+      list-style-position: inside;
+      margin-bottom: 20px;
+    }
+  }
+
+  .header {
+    text-align: center;
+  }
+
+  .title {
+    font-size: 1.7rem;
+    font-weight: 300;
+  }
+`;
 
 export default function Template({
   data // this prop will be injected by the GraphQL query below.
@@ -17,14 +47,23 @@ export default function Template({
 
   return (
     <AppWrapper>
-      <div className="blog-post-container">
-        <div className="blog-post">
-          <h1>{frontmatter.title}</h1>
-          <h2>{frontmatter.date}</h2>
-          {featuredImgFluid && <Img fluid={featuredImgFluid} />}
-          <MDXRenderer>{body}</MDXRenderer>
+      <StyledPost>
+        <div className="post">
+          <div className="blog-post">
+            <div className="header">
+              <h1 className="title">{frontmatter.title}</h1>
+              <div>
+                <h2>{frontmatter.date}</h2>
+                {/* TODO: Insert words here */}
+                {/* TODO: Insert time here */}
+              </div>
+            </div>
+
+            {featuredImgFluid && <Img fluid={featuredImgFluid} />}
+            <MDXRenderer>{body}</MDXRenderer>
+          </div>
         </div>
-      </div>
+      </StyledPost>
     </AppWrapper>
   );
 }
